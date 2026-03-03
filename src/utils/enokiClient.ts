9a797b12b29479f1e7a72b66185e04a6f43d4e26
incepthink <incepthink@gmail.com>
2026-03-03 21:11:11 +0530

    changes

--- src/utils/enokiClient.ts
+++ src/utils/enokiClient.ts
@@ -1,5 +1,5 @@
 import { EnokiClient } from "@mysten/enoki";
 
 export const enokiClient = new EnokiClient({
-  apiKey: "enoki_private_3bc20a19fe77af06b1deee9fc472bfbd",
-}); 
+  apiKey: "enoki_private_7d937074507bda2b093d68458dfbf049",
+});
